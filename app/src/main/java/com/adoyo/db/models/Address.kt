44fc52cc546a00a7854c0b50be79c7e319f4dc75
AdoyoClifford <adoyoclifford@gmail.com>
2024-02-07 21:20:57 +0300

    initial commit

---
+++ app/src/main/java/com/adoyo/db/models/Address.kt
@@ -0,0 +1,16 @@
+package com.adoyo.db.models
+
+import io.realm.kotlin.types.EmbeddedRealmObject
+
+//Teacher 1-to-1 Address
+//Teacher 1-to-many Course
+//Students many-to-many Course
+class Address: EmbeddedRealmObject {
+    var fullNAme: String = ""
+    var street: String = ""
+    var houseNumber: Int = 0
+    var zip: Int = 0
+    var city: String = ""
+    var teacher: Teacher? = null
+
+}
